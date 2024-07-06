ecfcb0599b00515f9ab43e7656182306617137cf
Satyajit Samal <samalsatyajit668@gmail.com>
2024-07-06 09:57:43 +0530

    Added all the files at once

---
+++ InheritanceClass/Calc.java
@@ -0,0 +1,12 @@
+package InheritanceClass;
+
+public class Calc {
+    // public int marks = 90;
+
+    public int add(int n1, int n2) {
+        return n1+n2;
+    }
+    public int sub(int n1, int n2) {
+        return n1-n2;
+    }
+}
---
+++ InheritanceClass/VeryAdvCalc.java
@@ -0,0 +1,7 @@
+package InheritanceClass;
+
+public class VeryAdvCalc extends AdvCalc{
+    public double power(int n1, int n2) {
+        return Math.pow(n1, n2);
+    }
+}
